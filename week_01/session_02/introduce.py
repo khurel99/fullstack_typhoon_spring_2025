aac95af2a89e318ec4e547885aac44a12c43004e
khurel <khurel9988@gmail.com>
2025-03-27 17:45:37 +0800

    session_2

---
+++ week_01/session_02/introduce.py
@@ -0,0 +1,10 @@
+# introduce myself
+# introduce yourself in three lines
+# for example : your firstname, lastname, age, gender
+# and occupation and hobbies
+
+print("Здравствуйте")
+print("Би 26 настай")
+print("I like to play table tennis")
+print("AIエンジニアとして４年間働きました。")
+print("I wrote it from Nano Editor")
